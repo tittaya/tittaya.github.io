ef33d9342ef40a026612263bdae7191dedc124f4
Tittaya <tittaya2542@gmail.com>
2020-02-12 12:13:31 +0700

    contact form validation

--- Profile/contact.js
+++ Profile/contact.js
@@ -1,7 +1,8 @@
-console.log("contact.js");  
+console.log("contact.js");
+
 var firebaseConfig = {
     apiKey: "AIzaSyDjz4UL_TbjWLbJxVb0DMJkXWreb7rGzbQ",
-    authDomain: "contactform-data.firebaseapp.com",
+    authDomain: 'localhost',
     databaseURL: "https://contactform-data.firebaseio.com",
     projectId: "contactform-data",
     storageBucket: "contactform-data.appspot.com",
@@ -11,35 +12,86 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-let db = firebase.firestore();
-let save = document.getElementById("save-button");
+const data = firebase.firestore();
+const contactList = document.getElementById("contact-list");
+
+function renderContact(doc){
+    let table = $('tbody')[0];
+    let row = table.insertRow(-1);
+    let firstCell = row.insertCell(0);
+    let secoundCell = row.insertCell(1);
+    let thirdCell = row.insertCell(2);
+    let fourthCell = row.insertCell(3);
+    firstCell.textContent = doc.data().FirstName;
+    secoundCell.textContent = doc.data().LastName;
+    thirdCell.textContent = doc.data().Gender;
+    fourthCell.textContent = doc.data().Email;
+}
+
+data.collection("users").get().then((snapshot) =>{
+    snapshot.docs.forEach(doc =>{
+        console.log(doc.data())
+        renderContact(doc);
+    });
+});
+const form = document.getElementById("contact-form");
 let reset = document.getElementById("reset-button");
-let firstName = document.getElementById("first-name");
-let lastName = document.getElementById("last-name");
-let detail = document.getElementById("detail");
-let gender = " ";
+
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if((form.firstName.value != '') && (form.lastName.value != '') && (form.email.value != '') 
+        && (form.gender.value != '') && (form.detail.value != '')){
+            data.collection("users").add({
+                FirstName: form.firstName.value,
+                LastName:  form.lastName.value,
+                Email: form.email.value,
+                Gender: form.gender.value,
+                Detail: form.detail.value
+            });
+            form.firstName.value = '';
+            form.lastName.value = '';
+            form.email.value = '';
+            form.gender.value = 'Male';
+            form.detail.value  = '';
+    }else{
+       if((form.firstName.value === '') && (form.lastName.value != '') && (form.email.value != '') 
+            && (form.gender.value != '') && (form.detail.value != '')){
+                alert('Please enter your first name.');
+            }else{
+                if((form.firstName.value != '') && (form.lastName.value === '') && (form.email.value != '') 
+                    && (form.gender.value != '') && (form.detail.value != '')){
+                        alert('Please enter your last name.');
+                    }else{
+                        if((form.firstName.value != '') && (form.lastName.value != '') && (form.email.value === '') 
+                            && (form.gender.value != '') && (form.detail.value != '')){
+                                alert('Please enter your E-mail.');
+                            }else{
+                                if((form.firstName.value != '') && (form.lastName.value != '') && (form.email.value != '') 
+                                    && (form.gender.value === '') && (form.detail.value != '')){
+                                        alert('Please select your gender');
+                                    }else{
+                                        if((form.firstName.value != '') && (form.lastName.value != '') && (form.email.value != '') 
+                                            && (form.gender.value != '') && (form.detail.value === '')){
+                                                alert('Please enter detail.');
+                                            }else{
+                                                alert('Please fill in every section of the form.');
+                                            }
+                                    }
+                            }
+                    }
+            }
+    }
+    
+})
 
 reset.addEventListener('click', function(){
-    firstName.value = " ";
-    lastName.value = " ";
-    gender.value = 1;
-    detail.value = " ";
+    form.firstName.value = '';
+    form.lastName.value = '';
+    form.email.value = '';
+    form.gender.value = 'Male';
+    form.detail.value  = '';
 });
 
-save.addEventListener('click', function(){
-    // if(document.getElementById('gender') == 1){
-    //     gender = "Male";
-    // }else{
-    //     if(document.getElementById('gender') == 2){
-    //         gender = "Female";
-    //     }else{
-    //         gender = "Others";
-    //     }
-    // }
-    db.collection("user").add({
-            FirstName: "ffffffffff",
-            LastName: "uuuuuuuuuu",
-            //Gender: gender.value(),
-        });
 
-});
+
