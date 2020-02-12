5fc27939c76d25d19135eb46e6f5ab5d16e291d5
Tittaya <tittaya2542@gmail.com>
2020-02-12 14:39:25 +0700

    contact finish

--- Profile/contact.js
+++ Profile/contact.js
@@ -15,29 +15,55 @@ firebase.initializeApp(firebaseConfig);
 const data = firebase.firestore();
 const contactList = document.getElementById("contact-list");
 
+let docID = '';
+
+
+//function for render contact list
 function renderContact(doc){
+    //let li = document.createElement('li');
     let table = $('tbody')[0];
     let row = table.insertRow(-1);
     let firstCell = row.insertCell(0);
     let secoundCell = row.insertCell(1);
     let thirdCell = row.insertCell(2);
     let fourthCell = row.insertCell(3);
+    let fifthCell = row.insertCell(4);
+
     firstCell.textContent = doc.data().FirstName;
     secoundCell.textContent = doc.data().LastName;
     thirdCell.textContent = doc.data().Gender;
-    fourthCell.textContent = doc.data().Email;
+
+    let str = doc.data().Email;
+    let emailSensored = '';
+    let i = 0;
+    i = str.indexOf('@');
+    let endIndex = i;
+    let startIndex = i+1
+    emailSensored = str.slice(0,1) + str.slice(1, endIndex).replace(/./g, 'x') +'@'+ str.slice(startIndex).replace(/./g, 'x');
+    fourthCell.textContent = emailSensored;
+
+    //let cross = document.createElement('div');
+    fifthCell.textContent = 'X';
+    fifthCell.setAttribute('type','button');
+    fifthCell.addEventListener('click', (e)=>{
+        e.stopPropagation();
+        data.collection('users').doc(docID).delete();
+    })
 }
 
+//render contact list
 data.collection("users").get().then((snapshot) =>{
     snapshot.docs.forEach(doc =>{
-        console.log(doc.data())
+        //console.log(doc.data())
+        
         renderContact(doc);
     });
 });
+
 const form = document.getElementById("contact-form");
 let reset = document.getElementById("reset-button");
 
-
+//save data to firebase
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     if((form.firstName.value != '') && (form.lastName.value != '') && (form.email.value != '') 
@@ -48,12 +74,18 @@ form.addEventListener('submit', (e)=>{
                 Email: form.email.value,
                 Gender: form.gender.value,
                 Detail: form.detail.value
-            });
+            })
+            .then(function(docRef){
+                console.log("Document written with ID: ", docRef.id);
+                docID = docRef.id
+            })
+            //console.log(sensorEmail(form.email.value));
             form.firstName.value = '';
             form.lastName.value = '';
             form.email.value = '';
             form.gender.value = 'Male';
             form.detail.value  = '';
+            
     }else{
        if((form.firstName.value === '') && (form.lastName.value != '') && (form.email.value != '') 
             && (form.gender.value != '') && (form.detail.value != '')){
@@ -85,6 +117,19 @@ form.addEventListener('submit', (e)=>{
     
 })
 
+// function sensorEmail(str){
+//     let i = 0;
+//     i = str.indexOf('@');
+//     let endIndex = i;
+//     return  str.slice(0,1) + str.slice(1, endIndex).replace(/./g, 'x') + str.slice(endIndex);
+// }
+
+//delete data
+function deleteData(doc){
+
+}
+
+//reset
 reset.addEventListener('click', function(){
     form.firstName.value = '';
     form.lastName.value = '';
